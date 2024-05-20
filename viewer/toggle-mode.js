39b99dc5678e8d5efdbd7a4df3559b00a50e3b3f
Biasiolo <biasiolorafael@gmail.com>
2024-05-20 14:23:13 -0300

    ajustes

--- viewer/toggle-mode.js
+++ viewer/toggle-mode.js
@@ -33,6 +33,10 @@
             });
         });
     };
+
+    
 }(jQuery));
 
+$('.toggle-switch input[type="checkbox"]').prop('checked', true); // Inicialize o botão no modo claro
 $('.toggle-switch').toggleSwitch(); // Inicialize o plugin nas classes 'toggle-switch' em seu HTML
+
